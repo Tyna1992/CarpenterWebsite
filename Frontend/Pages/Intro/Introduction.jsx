68b7d13152a417b856bd596504fd783883f94302
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-29 10:10:03 +0200

    feat: Introduction page skeleton created

---
+++ Frontend/Pages/Intro/Introduction.jsx
@@ -0,0 +1,14 @@
+﻿
+
+function Introduction() {
+  return (
+    <div className="intro" style={{marginLeft: "200px"}}>
+      <h1>Welcome to the React World</h1>
+      <p>
+        React is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies.
+      </p>
+    </div>
+  );
+}
+
+export default Introduction;
