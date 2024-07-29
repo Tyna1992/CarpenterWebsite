21f67dfd5ad3044a75fae8840e4842ac4a83817d
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-29 14:59:45 +0200

    feat: creating reusable button component

---
+++ Frontend/Components/Common/CommonButton/CommonButton.jsx
@@ -0,0 +1,16 @@
+﻿import Button from '@mui/material/Button';
+function CommonButton({children, color, disabled,size, variant, sx}){
+    return (
+        <Button 
+            color={color}
+            disabled={disabled}
+            size={size}
+            variant={variant}
+            sx={sx}
+        >
+            {children}
+        </Button>
+    )
+}
+
+export default CommonButton;
