77a32e4bf653087cd84d87e3964e851e6fa7f814
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-29 10:08:46 +0200

    feat: Navbar created

---
+++ Frontend/Components/NavBar/Navbar.jsx
@@ -0,0 +1,49 @@
+﻿// NavBar.jsx
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import navBarItems from './NavBarItems.jsx';
+import {useNavigate} from "react-router-dom";
+
+
+function NavBar() {
+    const drawerWidth = 200;
+    const navigate = useNavigate();
+    return (
+        <Drawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                    boxSizing: 'border-box',
+                },
+            }}
+            variant="permanent"
+            anchor="left"
+        >
+            <Toolbar />
+            <Divider />
+            <List>
+                {navBarItems.map((item) => (
+                    <ListItem key={item.id} disablePadding>
+                        <ListItemButton onClick={()=> navigate(item.route)}>
+                            <ListItemIcon>
+                                {item.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={item.label} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    );
+}
+
+export default NavBar;
