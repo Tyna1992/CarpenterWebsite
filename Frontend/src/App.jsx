96cb6b107bb115656a67e8ba72602fad27ccb74b
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-29 14:58:36 +0200

    feat: adding Grid as a container

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import Navbar from "../Components/NavBar/Navbar.jsx";
 import {Outlet} from "react-router-dom";
+import Grid from "@mui/material/Grid";
 
 
 function App() {
   
 
   return (
-    <>
+    <Grid container>
       <Navbar />
         <Outlet/>
-    </>
+    </Grid>
   )
 }
 
