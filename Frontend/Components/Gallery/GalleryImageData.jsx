87e86335c3dd09867c225b616fb26d6897eea80f
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-31 11:18:43 +0200

    feat: image data stored as an array of objects

---
+++ Frontend/Components/Gallery/GalleryImageData.jsx
@@ -0,0 +1,78 @@
+﻿import image1 from "../../src/assets/439417547_1103773007332083_3973944003719713992_n.jpg";
+import image2 from "../../src/assets/439276888_1103773003998750_3976265510815520847_n.jpg"
+
+export const kitchenImageData = [
+    {
+        id: 1,
+        img: image1,
+        title: 'Provance-i design konyha',
+        description: "A bézs színű gránit mosogató medence és a színazonos flexibilis csaptelep tökéletesen harmónizál a környezetével. " +
+            "A felső szekrények aljába meleg fényű érintés vezérelt LED-világítás került, így minden szeglet kellő megvilágítást kap."
+    },
+    {
+        id: 2,
+        img: image2,
+        title: 'Provance-i design konyha',
+        description: "A bútor színvilágához Egger U104 Alabástrom fehér színt választottunk, amihez színkód azonos festett frontok készültek, a munkalap pedig Kronospan K003 Arany Craft tölgy mintájú lett."
+    },
+    {
+        id: 3,
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+        description: "This is the description for image 3."
+    },
+    {
+        id: 4,
+        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        title: 'Coffee',
+        description: "This is the description for image 4."
+    },
+    {
+        id: 5,
+        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        title: 'Hats',
+        description: "This is the description for image 5."
+    },
+    {
+        id: 6,
+        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+        title: 'Honey',
+        description: "This is the description for image 6."
+    },
+    {
+        id: 7,
+        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+        title: 'Basketball',
+        description: "This is the description for image 7."
+    },
+    {
+        id: 8,
+        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+        title: 'Fern',
+        description: "This is the description for image 8."
+    },
+    {
+        id: 9,
+        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+        title: 'Mushrooms',
+        description: "This is the description for image 9."
+    },
+    {
+        id: 10,
+        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+        title: 'Tomato basil',
+        description: "This is the description for image 10."
+    },
+    {
+        id: 11,
+        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+        title: 'Sea star',
+        description: "This is the description for image 11."
+    },
+    {
+        id: 12,
+        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+        title: 'Bike',
+        description: "This is the description for image 12."
+    },
+];
