3ccee37a5eaa89ccada2155336a5a637b857691c
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-31 09:21:04 +0200

    fix: Unused import removed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from "../Components/NavBar/Navbar.jsx";
 import {Outlet} from "react-router-dom";
 import Grid from "@mui/material/Grid";
+import Home from "../Pages/Home/Home.jsx";
 
 
 function App() {
