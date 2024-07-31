bb3c432a20ca96cd0b926fefef4984270841d5da
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-31 11:20:49 +0200

    feat: The image rendering logic stored as a reusable component

---
+++ Frontend/Components/Gallery/GalleryImageList.jsx
@@ -0,0 +1,56 @@
+﻿import {ButtonBase, Dialog, DialogContent, DialogTitle, ImageList, ImageListItem, Typography} from "@mui/material";
+import React, {useState} from "react";
+
+
+function GalleryImageList({imageData=[]}){
+
+    const [open, setOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleClickOpen = (image) => {
+        setSelectedImage(image);
+        setOpen(true);
+    }
+    const handleClose = () => {
+        setOpen(false);
+        setSelectedImage(null);
+    }
+    
+    return(
+        <>
+        <ImageList variant="masonry" cols={3} gap={8}>
+            {imageData.map((image) => (
+                <ImageListItem key={image.id}>
+                    <ButtonBase onClick={() => handleClickOpen(image)}>
+                        <img
+                            src={image.img}
+                            alt={image.title}
+                            loading="eager"
+                            style={{ width: '100%', height: 'auto' }}
+                        />
+                    </ButtonBase>
+                </ImageListItem>
+            ))}
+        </ImageList>
+    <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{selectedImage?.title}</DialogTitle>
+        <DialogContent>
+            {selectedImage && (
+                <>
+                    <img
+                        src={selectedImage.img}
+                        alt={selectedImage.title}
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                    <Typography variant="body1" style={{ marginTop: '16px' }}>
+                        {selectedImage.description}
+                    </Typography>
+                </>
+            )}
+        </DialogContent>
+    </Dialog>
+        </>
+    )
+}
+
+export default GalleryImageList;
