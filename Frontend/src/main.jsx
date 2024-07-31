9616c908febef4631fe15cbe9eff49157f4f62f8
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-31 09:20:48 +0200

    feat: Components added to corresponding path

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,26 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Introduction from "../Pages/Intro/Introduction.jsx";
-
+import Gallery from "../Pages/Gallery/Gallery.jsx";
+import Home from "../Pages/Home/Home.jsx";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import theme from "../Theme/Theme.jsx";
+import Contact from "../Pages/Contact/Contact.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-     <Routes>
-         <Route path="/" element={<App />} >
-            <Route path="bemutatkozó" element={<Introduction/>}/>
-            <Route path="partnerek" element=""/>
-            <Route path="galéria" element=""/>
-            <Route path="árlista" element=""/>
-            <Route path="tájékozató" element=""/>
-            <Route path="bútor-kisokos" element=""/>
-            <Route path="hírek" element=""/>
-            <Route path="kapcsolat" element=""/>
-             <Route path="vélemények" element=""/>
-             
-         </Route>
-     </Routes>
-         
-    </BrowserRouter>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App/>}>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="bemutatkozó" element={<Introduction/>}/>
+                        <Route path="partnerek" element=""/>
+                        <Route path="galéria" element={<Gallery/>}/>
+                        <Route path="árlista" element=""/>
+                        <Route path="tájékozató" element=""/>
+                        <Route path="bútor-kisokos" element=""/>
+                        <Route path="hírek" element=""/>
+                        <Route path="kapcsolat" element={<Contact/>}/>
+                        <Route path="vélemények" element=""/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    </React.StrictMode>
 )
