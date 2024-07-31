8666cf175e07df34cc77a92d32ed4b35b226932e
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-31 09:19:22 +0200

    feat: Home page skeleton created

---
+++ Frontend/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+﻿import Grid from "@mui/material/Grid";
+
+function Home(){
+    return(
+        <Grid item sx={{paddingTop:"8rem"}}>
+            <h1>Home</h1>
+        </Grid>
+    )
+}
+
+export default Home;
