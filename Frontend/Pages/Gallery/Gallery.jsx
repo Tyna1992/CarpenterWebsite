b9920abe1d590fe949cbcf53667e4baf9d94fda1
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-02 10:36:37 +0200

    feat: second accordion added to show new furniture category images

--- Frontend/Pages/Gallery/Gallery.jsx
+++ Frontend/Pages/Gallery/Gallery.jsx
@@ -1,17 +1,19 @@
 ﻿import {
     Accordion,
     AccordionDetails,
-    AccordionSummary,
+    AccordionSummary, Divider,
 } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import {kitchenImageData} from "../../Components/Gallery/GalleryImageData.jsx";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import GalleryImageList from "../../Components/Gallery/GalleryImageList.jsx"
+import {RepairedImageList} from "../../Components/Gallery/GalleryImageData.jsx"
 
 function Gallery() {
     
     return (
-        <Grid item sx={{paddingTop: "8rem"}}>
+        <Grid container sx={{paddingTop: "10rem"}}>
+        <Grid item>
             <h1>Galéria</h1>
             <Accordion>
                 <AccordionSummary
@@ -19,12 +21,25 @@ function Gallery() {
                     aria-controls="panel1-content"
                     id="panel1-header"
                 >
-                  Konyhabútorok  
+                  Provance-i megjelenésű konyha 
                 </AccordionSummary>
                 <AccordionDetails>
                     <GalleryImageList imageData={kitchenImageData}/>
                 </AccordionDetails>
             </Accordion>
+            <Divider/>
+            <br/>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}
+                                  aria-controls="panel1-content"
+                                  id="panel1-header">
+                  Felújított  bútorok  
+                </AccordionSummary>
+                <AccordionDetails>
+                    <GalleryImageList imageData={RepairedImageList}/>
+                </AccordionDetails>
+            </Accordion>
+        </Grid>
         </Grid>
     );
 }
