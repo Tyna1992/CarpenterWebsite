e5e8a1c24f43b010182cddfc230015fc9bc05998
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-02 10:37:13 +0200

    fix: adding a Grid container as a wrapper to Introduction.jsx

--- Frontend/Pages/Intro/Introduction.jsx
+++ Frontend/Pages/Intro/Introduction.jsx
@@ -3,14 +3,16 @@
 
 function Introduction() {
   return (
-    <Grid item xs={8} sx={{paddingTop: "8rem"}}>
-      <h1>Kedves Látogató!</h1>
-      <p>
-          Kisgyerek korom óta foglalkozom az asztalossággal, 2020 nyarán pedig létrehoztam kis vállalkozásomat, igyekezve minden megrendelést, segítségkérést kielégíteni, legyen az bútor összeszerelés, javítás, kis ajándék készítés, vagy egy egész konyhabútor gyártása 🙂
-          Foglalkozom tömörfa bútorok, -tárgyak készítésével, valamint a laminált faforgácslapos termékek gyártása sem jelent akadályt!
-          Törekszem a magas minőségű termékek előállítására, mind küllemre, mind szerkezetileg egyaránt! 
-      </p>
-    </Grid>
+      <Grid container sx={{paddingTop: "10rem"}}>
+          <Grid item xs={8} sx={{backgroundColor: "rgba(216,161,101,0.6)", borderRadius: "8px"}}>
+              <h1>Kedves Látogató!</h1>
+              <p>
+                  Kisgyerek korom óta foglalkozom az asztalossággal, 2020 nyarán pedig létrehoztam kis vállalkozásomat, igyekezve minden megrendelést, segítségkérést kielégíteni, legyen az bútor összeszerelés, javítás, kis ajándék készítés, vagy egy egész konyhabútor gyártása 🙂
+                  Foglalkozom tömörfa bútorok, -tárgyak készítésével, valamint a laminált faforgácslapos termékek gyártása sem jelent akadályt!
+                  Törekszem a magas minőségű termékek előállítására, mind küllemre, mind szerkezetileg egyaránt!
+              </p>
+          </Grid>
+      </Grid>
   );
 }
 
