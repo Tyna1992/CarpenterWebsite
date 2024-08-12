6cecacdae533d4c4bd5467dc0647840b7b226463
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-12 14:31:38 +0200

    feat: Suppliers.jsx created to display the suppliers of used materials

---
+++ Frontend/Pages/Partners/Suppliers.jsx
@@ -0,0 +1,12 @@
+﻿import Grid from "@mui/material/Grid";
+
+
+function Suppliers() {
+  return (
+    <Grid container sx={{ paddingTop: "9rem" }}>
+      <h1>Partnerek</h1>
+    </Grid>
+  );
+}
+
+export default Suppliers;
