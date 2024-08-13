31864f4f38fc2cdca26626d8d8fed1993b676a10
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-13 15:58:16 +0200

    feat: Created a simple footer

---
+++ Frontend/src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+﻿import {Box, Link, Typography} from "@mui/material";
+
+function Footer() {
+    return (
+        <Box
+            component="footer"
+            sx={{
+                py: 1,
+                px: 2,
+                mt: 'auto',
+                backgroundColor: (theme) =>
+                    theme.palette.mode === 'light' ? 'rgba(136,108,68,0.85)' : 'rgba(255, 255, 255, 0.1)',
+                textAlign: 'center',
+                height: "4rem",
+                paddingTop: "1rem",
+            }}
+        >
+            <Typography variant="body1" color="text.primary">
+                © 2024 Szanyi Bálint Asztalos
+            </Typography>
+            
+        </Box>
+    );
+}
+
+export default Footer;
