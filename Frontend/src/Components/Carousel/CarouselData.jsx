018f031b27d8b2ec47f9ca0c0011f7094253ff2f
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-14 18:40:10 +0200

    feat: const file created to store all the data for the carousel

---
+++ Frontend/src/Components/Carousel/CarouselData.jsx
@@ -0,0 +1,38 @@
+﻿import clock from "../../assets/CarouselImages/colck.jpg"
+import birdfeeder from "../../assets/CarouselImages/birdfeeder.jpg"
+import kitchen1 from "../../assets/CarouselImages/kitchen_gray.jpg"
+import box from "../../assets/CarouselImages/box.jpg"
+import houses from "../../assets/CarouselImages/houses.jpg"
+
+
+import pendrive from "../../assets/CarouselImages/pendrive.jpg"
+
+
+export const carouselData = [
+    {
+        image: clock,
+        description: "",
+        alt: "clock"
+    },
+    {
+        image: pendrive,
+        description: "",
+        alt: "pendrive"
+    },
+    {
+        image: birdfeeder,
+        description: "",
+        alt: "birdfeeder"
+    },
+    {
+        image: box,
+        description: "",
+        alt: "box"
+    },
+    {
+        image: houses,
+        description: "",
+        alt: "houses"
+    }
+    
+]
