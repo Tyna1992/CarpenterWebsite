33a9dbd6af6a2a6aa9df3b9e27db9c19ec7f0455
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-26 22:41:55 +0200

    feat: Context file created, entities relationships configured

---
+++ Website/CarpenterServer/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using CarpenterServer.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarpenterServer.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<Blogpost> Blogposts { get; set; }
+    public DbSet<Pricelist> Pricelists { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Blogpost>().HasIndex(post => post.Title).IsUnique();
+        builder.Entity<Pricelist>().HasIndex(list => list.Job).IsUnique();
+        builder.Entity<Review>().HasIndex(review => new { review.Name, review.Email }).IsUnique();
+
+
+        base.OnModelCreating(builder);
+    }
+}
