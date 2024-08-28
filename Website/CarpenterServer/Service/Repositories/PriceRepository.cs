b5bc749d136d917d73c9f737489a011a88ea81b6
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-28 09:33:51 +0200

    feat: Interface fully implemented, CRUD operations established

--- Website/CarpenterServer/Service/Repositories/PriceRepository.cs
+++ Website/CarpenterServer/Service/Repositories/PriceRepository.cs
@@ -1,31 +1,55 @@
-﻿using CarpenterServer.Model;
+﻿using CarpenterServer.Data;
+using CarpenterServer.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarpenterServer.Service.Repositories;
 
 public class PriceRepository : IPriceRepository
 {
+    
+    private readonly DataContext _context;
+    
+    public PriceRepository(DataContext context)
+    {
+        _context = context;
+    }
+    
     public async Task<IEnumerable<Pricelist>> GetAllPrices()
     {
-        throw new NotImplementedException();
+        return await _context.Pricelists.ToListAsync();
     }
 
     public async Task<Pricelist> GetPriceByJob(string job)
     {
-        throw new NotImplementedException();
+        var price = await _context.Pricelists.FindAsync(job);
+        return price;
     }
 
     public async Task<Pricelist> AddPrice(Pricelist price)
     {
-        throw new NotImplementedException();
+        await _context.Pricelists.AddAsync(price);
+        await _context.SaveChangesAsync();
+        return price;
     }
 
     public async Task<Pricelist> UpdatePrice(string job, Pricelist price)
     {
-        throw new NotImplementedException();
+        var existingPrice = await _context.Pricelists.FindAsync(job);
+        if (existingPrice != null)
+        {
+            existingPrice.Price = price.Price;
+            await _context.SaveChangesAsync();
+        }
+        return existingPrice;
     }
 
     public async Task DeletePrice(string job)
     {
-        throw new NotImplementedException();
+        var price = await _context.Pricelists.FindAsync(job);
+        if (price != null)
+        {
+            _context.Pricelists.Remove(price);
+            await _context.SaveChangesAsync();
+        }
     }
 }
