8360c1ee59507d87474a22a65295ef51687e7357
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-28 09:28:30 +0200

    feat: Controllers skeleton created for prices

---
+++ Website/CarpenterServer/Controllers/PriceController.cs
@@ -0,0 +1,15 @@
+﻿using CarpenterServer.Service.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpenterServer.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class PriceController : ControllerBase
+{
+    public readonly IPriceRepository _priceRepository;
+    
+    public PriceController(IPriceRepository priceRepository)
+    {
+        _priceRepository = priceRepository;
+    }
+}
