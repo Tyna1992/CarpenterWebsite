56be745e55396c752fbbf9bf8244700b11eb2239
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-28 09:27:08 +0200

    feat: Repository scaffolded for prices

---
+++ Website/CarpenterServer/Service/Repositories/PriceRepository.cs
@@ -0,0 +1,31 @@
+﻿using CarpenterServer.Model;
+
+namespace CarpenterServer.Service.Repositories;
+
+public class PriceRepository : IPriceRepository
+{
+    public async Task<IEnumerable<Pricelist>> GetAllPrices()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Pricelist> GetPriceByJob(string job)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Pricelist> AddPrice(Pricelist price)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Pricelist> UpdatePrice(string job, Pricelist price)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeletePrice(string job)
+    {
+        throw new NotImplementedException();
+    }
+}
