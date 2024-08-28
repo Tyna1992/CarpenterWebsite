edcdda25d989fecc1aff8af026bb16d9331833ba
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-28 13:41:12 +0200

    fix: UpdatePrice methods parameters changed

--- Website/CarpenterServer/Service/Repositories/Prices/IPriceRepository.cs
+++ Website/CarpenterServer/Service/Repositories/Prices/IPriceRepository.cs
@@ -7,7 +7,7 @@ public interface IPriceRepository
     public Task<IEnumerable<Pricelist>> GetAllPrices();
     public Task<Pricelist> GetPriceByJob(string job);
     public Task<Pricelist> AddPrice(Pricelist price);
-    public Task<Pricelist> UpdatePrice(string job, Pricelist price);
+    public Task<Pricelist> UpdatePrice(string job, decimal price);
     public Task DeletePrice(string job);
     
 }
