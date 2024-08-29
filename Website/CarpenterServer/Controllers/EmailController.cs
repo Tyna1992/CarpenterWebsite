a791be2be052390be1f950500820f56404d49d43
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-08-29 14:18:37 +0200

    feat: Controller for getting email from user created

---
+++ Website/CarpenterServer/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using CarpenterServer.Contracts;
+using CarpenterServer.Service.EmailService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpenterServer.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class EmailController : ControllerBase
+{
+    private readonly IEmailService _emailService;
+    
+    public EmailController(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+    
+    [HttpPost("sendOfferRequestEmail")]
+    public async Task<IActionResult> SendOfferRequestEmail([FromBody] OfferRequestEmail request)
+    {
+        try
+        {
+            Console.WriteLine(request.fromEmail);
+            await _emailService.SendOfferRequestEmailAsync(request.fromEmail, request.name, request.phone, request.message);
+            
+            return Ok("Email sent successfully.");
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+}
