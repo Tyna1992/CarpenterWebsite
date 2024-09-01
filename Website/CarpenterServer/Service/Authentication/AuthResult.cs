46cc1defa87e260021cbfe30f782799a5e9d40b1
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-01 21:46:33 +0200

    feat: Record for the result of the authentication process created

---
+++ Website/CarpenterServer/Service/Authentication/AuthResult.cs
@@ -0,0 +1,10 @@
+﻿namespace CarpenterServer.Service.Authentication;
+
+public record AuthResult(
+    bool Success,
+    string Email,
+    string Token)
+{
+    //Error code - error message
+    public readonly Dictionary<string, string> ErrorMessages = new();
+}
