5f1cdd2ed5e371f2967d8c5a790221d09ef3de33
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-01 14:37:15 +0200

    feat: data fetching logic implemented

--- Frontend/src/Pages/Pricelist/Pricelist.jsx
+++ Frontend/src/Pages/Pricelist/Pricelist.jsx
@@ -1,7 +1,25 @@
 ﻿import Grid from "@mui/material/Grid";
 import RequestOffer from "../../Components/Forms/RequestOffer.jsx";
+import {useEffect, useState} from "react";
 
 function Pricelist() {
+    
+    const [prices, setPrices] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async()=>{
+            try {
+                const response = await fetch("/api/Price/GetAllPrices");
+                const data = await response.json();
+                console.log(data);
+                setPrices(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchData();
+    }, []);
+    
     return (
         <Grid container sx={{ paddingTop: "10rem" }} >
             <Grid item xs={12}  >
@@ -15,23 +33,12 @@ function Pricelist() {
             <Grid container item xs={12} spacing={2} >
                 <Grid item xs={12} sm={6} style={{backgroundColor: "rgba(216,161,101,0.6)"}}>
                     <ul style={{listStyleType: "none"}}>
-                        <li>Asztalos munkadíj: 5,000 HUF/óra</li>
-                        <br/>
-                        <li>Lapraszerelt bútor összeszerelés: 15,000 HUF-tól</li>
-                        <br/>
-                        <li>Egyedi bútortervezés: 25,000 HUF-tól</li>
-                        <br/>
-                        <li>Bútorkészítés (szék): 20,000 HUF-tól</li>
-                        <br/>   
-                        <li>Bútorkészítés (asztal): 35,000 HUF-tól</li>
-                        <br/>
-                        <li>Polcrendszer készítése: 30,000 HUF-tól</li>
-                        <br/>
-                        <li>Ajtókeret készítése: 18,000 HUF-tól</li>
-                        <br/>
-                        <li>Konyhabútor tervezés és kivitelezés: 150,000 HUF-tól</li>
-                        <br/>
-                        <li>Étkező bútor tervezés és kivitelezés: 120,000 HUF-tól</li>
+                        {prices.map((item) => {
+                            return (
+                                <li key={item.id} style={{ marginBottom: '10px' }}>{item.job}: {item.price} HUF-tól</li>
+                                
+                            )
+                        })}
                     </ul>
                 </Grid>
                 <RequestOffer/>
