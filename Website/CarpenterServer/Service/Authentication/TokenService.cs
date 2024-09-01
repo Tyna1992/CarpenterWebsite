d39f02de86212b91a2766fc8c789584a558aa889
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-01 21:47:19 +0200

    feat: Service for generating JWT tokens created

---
+++ Website/CarpenterServer/Service/Authentication/TokenService.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CarpenterServer.Model;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CarpenterServer.Service.Authentication;
+
+public class TokenService : ITokenService
+{
+    private readonly IConfiguration _configuration;
+    private const int ExpirationMinutes = 60;
+    
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public string CreateToken(Admin user, string role)
+    {
+        var expiration = DateTime.UtcNow.AddMinutes(ExpirationMinutes);
+        var token = CreateJwtToken(
+            CreateClaims(user, role),
+            CreateSigningCredentials(),
+            expiration
+        );
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+    
+    private JwtSecurityToken CreateJwtToken(List<Claim> claims, SigningCredentials credentials,
+        DateTime expiration) =>
+        new(
+            _configuration.GetSection("JWTSettings").Get<JWTSettings>().ValidIssuer,
+            _configuration.GetSection("JWTSettings").Get<JWTSettings>().ValidAudience,
+            claims,
+            expires: expiration,
+            signingCredentials: credentials
+        );
+    
+    private List<Claim> CreateClaims(Admin admin, string? role)
+    {
+        try
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "TokenForTheApiWithAuth"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)),
+                new Claim(ClaimTypes.NameIdentifier, admin.Id),
+                new Claim(ClaimTypes.Email, admin.Email),
+            };
+
+            if (role != null)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    private SigningCredentials CreateSigningCredentials()
+    {
+        return new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("IssuerSigningKey").Value)
+            ),
+            SecurityAlgorithms.HmacSha256
+        );
+    }
+}
