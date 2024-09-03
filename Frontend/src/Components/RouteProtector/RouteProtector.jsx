975d492e9eab8bf5e55f6d2322603a9b05db4682
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-03 11:03:07 +0200

    feat: Route protector component created to restrict access to specific routes

---
+++ Frontend/src/Components/RouteProtector/RouteProtector.jsx
@@ -0,0 +1,25 @@
+﻿
+import {Navigate, useNavigate} from "react-router-dom";
+import {UserContext} from "../Context/UserContext.jsx";
+import {useContext} from "react";
+
+
+function RouteProtector({item}) {
+    const {user, setUser} = useContext(UserContext);
+        
+    if(user === null)
+    {
+        return <div>Loading...</div>;
+    }
+    
+    if(user.username === "admin")
+    {
+        return item;
+    }
+
+    return <Navigate to="/" />;
+    
+    
+}
+
+export default RouteProtector;
