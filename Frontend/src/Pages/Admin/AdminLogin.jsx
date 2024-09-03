34ec59517b384029dcb35f636350a498b42f68d4
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-03 11:09:55 +0200

    feat: Login form displayed on the AdminLogin page

---
+++ Frontend/src/Pages/Admin/AdminLogin.jsx
@@ -0,0 +1,18 @@
+﻿import Grid from "@mui/material/Grid";
+import AdminLoginForm from "../../Components/Forms/AdminLoginForm.jsx";
+
+
+function AdminLogin(){   
+    
+    return(
+        <Grid container sx={{paddingTop: "10rem"}}>
+            <Grid item xs="auto" sx={{backgroundColor: "rgba(216,161,101,0.6)", borderRadius: "8px"}}>
+                <h1>Admin Login</h1>
+                <AdminLoginForm />
+            </Grid>
+        </Grid>
+
+    )
+}
+
+export default AdminLogin;
