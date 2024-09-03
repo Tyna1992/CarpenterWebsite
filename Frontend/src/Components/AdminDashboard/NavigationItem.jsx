21b6f176dea5ee388e1d30d612b8731079e29ff5
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-03 12:35:14 +0200

    feat: navigation items created for admin dashboard

---
+++ Frontend/src/Components/AdminDashboard/NavigationItem.jsx
@@ -0,0 +1,52 @@
+﻿import PriceChangeIcon from '@mui/icons-material/PriceChange';
+import HandshakeIcon from '@mui/icons-material/Handshake';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import ThumbsUpDownIcon from '@mui/icons-material/ThumbsUpDown';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import InfoIcon from '@mui/icons-material/Info';
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+export const NavigationItem = [
+    {
+        id: 9,
+        icon: <PriceChangeIcon/>,
+        label: "Árlista szerkesztése",
+        route: "adminArlistaSzerk"
+    },
+    {
+        id: 10,
+        icon: <HandshakeIcon/>,
+        label: "Partnerek szerkesztése",
+        route: "adminPartnerekSzerk"        
+    },
+    {
+        id: 11,
+        icon: <ContactPhoneIcon/>,
+        label: "Kontaktinfó szerkesztése",
+        route: "adminKontaktSzerk"        
+    },
+    {
+        id: 12,
+        icon: <ThumbsUpDownIcon/>,
+        label: "Értékelések hitelesítése",
+        route: "adminErtekelesekCheck"
+    },
+    {
+        id: 13,
+        icon: <NewspaperIcon/>,
+        label: "Hírek szerkesztése",
+        route: "adminHirekSzerk"        
+    },
+    {
+        id: 14,
+        icon: <InfoIcon/>,
+        label: "Tájékoztató szerkesztése",
+        route: "adminTajekozatoSzerk"        
+    },
+    {
+        id: 15,
+        icon: <TipsAndUpdatesIcon/>,
+        label: "Blog szerkesztése",
+        route: "adminBlogSzerk"        
+    }
+    
+];
