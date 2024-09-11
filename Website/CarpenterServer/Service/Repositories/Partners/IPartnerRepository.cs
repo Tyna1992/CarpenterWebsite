92303e297fc542ea756b68ab415528fbc5429832
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-11 14:02:37 +0200

    fix: method signatures changed

--- Website/CarpenterServer/Service/Repositories/Partners/IPartnerRepository.cs
+++ Website/CarpenterServer/Service/Repositories/Partners/IPartnerRepository.cs
@@ -7,7 +7,7 @@ public interface IPartnerRepository
     public Task<IEnumerable<Partner>> GetAllPartners();
     public Task<Partner> GetPartnerByName(string name);
     public Task<Partner> AddPartner(Partner partner);
-    public Task<Partner> UpdatePartner(string name, Partner partner);
-    public Task<Partner?> DeletePartner(string name);
+    public Task<Partner> UpdatePartner( Partner partner);
+    public Task<Partner?> DeletePartner(string id);
     
 }
