ae513dc8edfba143cd3e7a8feb17aecfadc42088
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-12 22:13:56 +0200

    feat: Controller created to get, add and update reviews

---
+++ Website/CarpenterServer/Controllers/ReviewController.cs
@@ -0,0 +1,47 @@
+﻿using CarpenterServer.Contracts;
+using CarpenterServer.Model;
+using CarpenterServer.Service.Repositories.Reviews;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpenterServer.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReviewController : ControllerBase
+{
+    private readonly IReviewRepository _reviewRepository;
+    
+    public ReviewController(IReviewRepository reviewRepository)
+    {
+        _reviewRepository = reviewRepository;
+    }
+    
+    [HttpGet("GetAll")]
+    public async Task<ActionResult<IEnumerable<Review>>> GetReviews()
+    {
+        var reviews = await _reviewRepository.GetReviewsAsync();
+        if (!reviews.Any())
+        {
+            return NotFound("No reviews found.");
+        }
+
+        return Ok(reviews);
+    }
+    
+    [HttpPut("Verify"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> VerifyReview([FromBody] Review request)
+    {
+        Console.WriteLine(request.Id +" "+ request.Verified + request.Name);
+        var result = await _reviewRepository.VerifyReviewAsync(request.Id.ToString(), request.Verified);
+        return Ok(result);
+    }
+    
+    [HttpPost("Add")]
+    public async Task<ActionResult> AddReview([FromBody]Review review)
+    {
+        var newReview = await _reviewRepository.AddReviewAsync(review);
+        return CreatedAtAction(nameof(GetReviews), newReview);
+    }
+}
