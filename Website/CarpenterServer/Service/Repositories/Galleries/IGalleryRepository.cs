b13d43de95503dc4d487613322695ea02ad225a7
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-22 22:02:14 +0200

    feat: repository layer implemented for galleries

---
+++ Website/CarpenterServer/Service/Repositories/Galleries/IGalleryRepository.cs
@@ -0,0 +1,11 @@
+﻿using CarpenterServer.Model;
+
+namespace CarpenterServer.Service.Repositories.Galleries;
+
+public interface IGalleryRepository
+{
+    Task AddGallery(Gallery gallery);
+    Task<IEnumerable<Gallery>> GetAllGalleries();
+    Task<Gallery> GetGalleryById(Guid id);
+    Task DeleteGallery(Guid id);
+}
