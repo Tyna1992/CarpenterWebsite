c67b9b58f4001d569621c4dce315df6472ce635d
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-22 21:57:52 +0200

    feat: entity model created for image-galleries

---
+++ Website/CarpenterServer/Model/Gallery.cs
@@ -0,0 +1,8 @@
+﻿namespace CarpenterServer.Model;
+
+public class Gallery
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<ImageEntity> Images { get; set; } = new List<ImageEntity>();
+}
