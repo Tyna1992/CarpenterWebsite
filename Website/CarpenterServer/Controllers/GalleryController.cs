88279f0753c8c79e37348b9a91c9e726274fb6b3
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-09-27 10:31:24 +0200

    feat: gallery creating endpoint implemented

---
+++ Website/CarpenterServer/Controllers/GalleryController.cs
@@ -0,0 +1,33 @@
+﻿using CarpenterServer.Model;
+using CarpenterServer.Service.Repositories.Galleries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpenterServer.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class GalleryController : ControllerBase
+{
+    private readonly IGalleryRepository _galleryRepository;
+    
+    public GalleryController(IGalleryRepository galleryRepository)
+    {
+        _galleryRepository = galleryRepository;
+    }
+    
+    [HttpPost("AddGallery")]
+    public async Task<ActionResult> AddGallery([FromBody] Gallery gallery)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        await _galleryRepository.AddGallery(gallery);
+        
+        foreach (var image in gallery.Images)
+        {
+            image.GalleryId = gallery.Id; 
+        }
+        return Ok(gallery);
+    }
+}
