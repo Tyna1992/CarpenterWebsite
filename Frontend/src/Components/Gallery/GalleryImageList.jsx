14e2478634e677b6465878b264ca8d82a7e58b56
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-10-10 17:31:46 +0200

    feat: image deleting logic implemented

--- Frontend/src/Components/Gallery/GalleryImageList.jsx
+++ Frontend/src/Components/Gallery/GalleryImageList.jsx
@@ -1,12 +1,17 @@
-﻿import {ButtonBase, Dialog, DialogContent, DialogTitle, ImageList, ImageListItem, Typography} from "@mui/material";
+﻿import {ButtonBase, Dialog, DialogContent, DialogTitle, ImageList, ImageListItem, Typography, Button, DialogActions} from "@mui/material";
 import React, {useState} from "react";
+import ConfirmDialog from "../AdminDashboard/ConfirmDialog.jsx";
+import notify from "../Notifications/Notify.jsx";
 
 
 function GalleryImageList({imageData=[]}){
 
     const [open, setOpen] = useState(false);
     const [selectedImage, setSelectedImage] = useState(null);
+    const [confirmOpen, setConfirmOpen] = useState(false);
+    
 
+    
     const handleClickOpen = (image) => {
         setSelectedImage(image);
         setOpen(true);
@@ -15,6 +20,38 @@ function GalleryImageList({imageData=[]}){
         setOpen(false);
         setSelectedImage(null);
     }
+
+    const handleDeleteClick = () => {
+        setConfirmOpen(true);  
+    };
+
+    const handleConfirmDelete = async () => {
+        try {
+            const response = await fetch(`/api/Image/delete/${selectedImage.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                notify("Image deleted successfully", "success");
+                const updatedGalleries = galleries.map((gallery) => ({
+                    ...gallery,
+                    images: gallery.images.filter((image) => image.id !== selectedImage.id),
+                }));
+                setGalleries(updatedGalleries);
+                setOpen(false);
+            } else {
+                notify("Error deleting image", "error");
+            }
+        } catch (error) {
+            notify("Error deleting image", "error");
+            console.error("Error:", error);
+        }
+        setConfirmOpen(false); 
+    };
+    
     
     return(
         <>
